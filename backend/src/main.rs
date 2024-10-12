8d8858321a2afcf383fc8151a6f8360c642fb8b1
Heshan Padmasiri <hpheshan@gmail.com>
2024-10-12 07:38:50 +0530

    Wired up front end and backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,34 @@
+use actix_cors::Cors;
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TestResponse {
+    message: String,
+}
 
 #[get("/")]
 async fn test() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    println!("test called");
+    HttpResponse::Ok().json(TestResponse {
+        message: "hello world from test".to_owned(),
+    })
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(test))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .wrap(
+                Cors::default()
+                    .allow_any_origin()
+                    .allow_any_method()
+                    .allow_any_header()
+                    .max_age(3600),
+            )
+            .service(test)
+    })
+    .bind(("127.0.0.1", 9090))?
+    .run()
+    .await
 }
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,5 +1,34 @@
+use leptos::leptos_dom::logging::console_log;
+use leptos::prelude::*;
 use leptos::*;
+use std::collections::HashMap;
+
+#[component]
+pub fn Test() -> impl IntoView {
+    let (message, set_message) = create_signal(String::from("initial value"));
+    create_effect(move |_| {
+        spawn_local(async move {
+            match get_test_str().await {
+                Ok(result) => set_message(result),
+                Err(message) => set_message(format!("failed to get message due to: {:?}", message)),
+            };
+        });
+    });
+    view! {<p>  {message}</p>}
+}
+
+pub async fn get_test_str() -> Result<String, String> {
+    console_log("test");
+    let resp = reqwest::get("http://127.0.0.1:9090")
+        .await
+        .map_err(|err| format!("failed to get response due to {:?}", err))?
+        .json::<HashMap<String, String>>()
+        .await
+        .map_err(|err| format!("failed to databind message due to {:?}", err))?;
+    console_log(&format!("{:?}", &resp));
+    Ok(resp["message"].clone())
+}
 
 fn main() {
-    mount_to_body(|| view! { <p>  "Hello, World!"  </p>})
+    mount_to_body(|| view! { <Test/>})
 }
