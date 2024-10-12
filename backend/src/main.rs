880ec86b07f02cc4cb1f7cf15a4a18a8c37396d7
Heshan Padmasiri <hpheshan@gmail.com>
2024-10-12 08:10:56 +0530

    Introduce hard coded collections

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,37 @@
 use actix_cors::Cors;
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[derive(Clone)]
+struct Context {}
+
+impl Context {
+    async fn get_collections(&self) -> Vec<common::CollectionIdentifier> {
+        vec![common::CollectionIdentifier {
+            name: "test collection".to_owned(),
+            index: 1,
+        }]
+    }
+}
 
 #[get("/")]
-async fn test() -> impl Responder {
+async fn test(_cx: web::Data<Context>) -> impl Responder {
     println!("test called");
     HttpResponse::Ok().json(common::TestResponse {
         message: "hello world from test".to_owned(),
     })
 }
 
+// TODO: figure out how to pass in a context here
+#[get("/collections")]
+async fn collections(cx: web::Data<Context>) -> impl Responder {
+    let cx = cx.get_ref();
+    let collections = cx.get_collections().await;
+    HttpResponse::Ok().json(collections)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
             .wrap(
                 Cors::default()
@@ -20,7 +40,9 @@ async fn main() -> std::io::Result<()> {
                     .allow_any_header()
                     .max_age(3600),
             )
+            .app_data(web::Data::new(Context {}))
             .service(test)
+            .service(collections)
     })
     .bind(("127.0.0.1", 9090))?
     .run()
--- common/src/lib.rs
+++ common/src/lib.rs
@@ -4,3 +4,26 @@ use serde::{Deserialize, Serialize};
 pub struct TestResponse {
     pub message: String,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum FileKind {
+    Image,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct File {
+    pub name: String,
+    pub index: u32,
+    pub kind: FileKind,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Collection {
+    pub files: Vec<File>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CollectionIdentifier {
+    pub name: String,
+    pub index: u32,
+}
